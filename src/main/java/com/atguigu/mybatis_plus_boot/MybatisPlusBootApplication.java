51eac042d7db7066ad0a1787fd65821f83e26d43
changchen <doublechang123@163.com>
2019-07-11 12:25:19 +0800

    主程序修改

--- src/main/java/com/atguigu/mybatis_plus_boot/MybatisPlusBootApplication.java
+++ src/main/java/com/atguigu/mybatis_plus_boot/MybatisPlusBootApplication.java
@@ -1,11 +1,9 @@
 package com.atguigu.mybatis_plus_boot;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.atguigu.mybatis_plus_boot.mapper")
 public class MybatisPlusBootApplication {
 
     public static void main(String[] args) {
