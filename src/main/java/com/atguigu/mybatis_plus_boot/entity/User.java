96828b73cfb1987e4e1ac106449bcb358a61c132
changchen <doublechang123@163.com>
2019-07-11 09:59:50 +0800

    修改实体类的主键id生成策略

--- src/main/java/com/atguigu/mybatis_plus_boot/entity/User.java
+++ src/main/java/com/atguigu/mybatis_plus_boot/entity/User.java
@@ -5,9 +5,14 @@ import lombok.Data;
 /**
  * @author Administrator
  * @create 2019-07-10 11:49
+ * mybatis_plus 默认的主键策略 ID_WORKER
  */
 @Data
 public class User {
+
+    //主键策略 ID_WORKER
+    //主键自增 在数据库 需要设置主键自增
+//    @TableId(type = IdType.AUTO)
     private Long id;
     private String name;
     private Integer age;
