eda8c2023c3b8a82b54375575a84eab8d224492c
changchen <doublechang123@163.com>
2019-07-12 09:36:36 +0800

    添加注解

--- src/main/java/com/atguigu/mybatis_plus_boot/mapper/UserMapper.java
+++ src/main/java/com/atguigu/mybatis_plus_boot/mapper/UserMapper.java
@@ -2,10 +2,12 @@ package com.atguigu.mybatis_plus_boot.mapper;
 
 import com.atguigu.mybatis_plus_boot.entity.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Administrator
  * @create 2019-07-10 13:05
  */
+@Repository
 public interface UserMapper extends BaseMapper<User> {
 }
